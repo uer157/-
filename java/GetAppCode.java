5913fd99b26cdbd73322084c66a357d7fb5a2880
uer157 <89568725+uer157@users.noreply.github.com>
2022-07-25 10:42:11 +0800

    Create GetAppCode.java

---
+++ java/GetAppCode.java
@@ -0,0 +1,21 @@
+public class GetAppCode
+//获取versionCoud
+{
+public long getCode{
+long appVersionCode = 0;
+        try {
+            PackageInfo packageInfo = context.getApplicationContext()
+                    .getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                appVersionCode = packageInfo.getLongVersionCode();
+            } else {
+                appVersionCode = packageInfo.versionCode;
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e("", e.getMessage());
+        }
+        return appVersionCode;
+    }
+}
+}
